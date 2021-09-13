43423fa36e2d2c2a9f4bba18a2dca3cf71ea4bfb
ValentinZC <ipokerby@gmail.com>
2021-09-13 13:45:47 +0300

    add: count-cat task finish

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let number = 0;
+  matrix.forEach(el => el.forEach(el => el === '^^' ? number++ : null));
+  return number ? number : 0;
 }
