5bd2f9011ae0dc362b87711cba1a7449cfbe478c
ValentinZC <ipokerby@gmail.com>
2021-09-13 14:04:18 +0300

    add: dream-team finish task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let teamNickName = [];
+
+  members.forEach(name => typeof name === 'string' ? teamNickName.push(name.toUpperCase().trim().charAt(0)) : 0);
+
+  return teamNickName.sort((a, b) => a + b).join('');
 }
