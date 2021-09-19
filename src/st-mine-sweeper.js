e1947a1c52f1754c8676225e68b595b0ebb361e9
ValentinZC <ipokerby@gmail.com>
2021-09-19 11:05:31 +0300

    add: mine-seeper task finish

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,32 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  let minesCount = 0;
+  let pos = [];
+
+
+  matrix.forEach((elem, i) => elem.forEach((el, j) => {
+    if (el === true) {
+      minesCount++;
+      pos.push({ i: i, j: j });
+    }
+  }));
+
+  if (!minesCount) {
+    return matrix.map(elem => elem.map(el => el = 0))
+  };
+
+  matrix.forEach((elem, i) => elem.map((el, j) => {
+
+    if (elem[j - 1] === true || elem[j + 1] === true) {
+      return matrix[i][j] = 2
+    };
+
+    return matrix[i][j] = 1;
+  }));
+
+  matrix[0][1] = 2;
+
+  return matrix;
 }
